43a07eaca35b5c530d784784c9459dcf62de345e
Adam Fedor <afedor@t1v.com>
2025-04-16 09:34:56 -0600

    Update product and target

--- Package.swift
+++ Package.swift
@@ -3,4 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftWebSocket"
+    products: [
+        .library(
+            name: "SwiftWebSocket",
+            targets: ["SwiftWebSocket-iOS"]),
+    ],
+        targets: [
+        .target(
+            name: "SwiftWebSocket-iOS",
+            dependencies: []),
+    ]
 )
